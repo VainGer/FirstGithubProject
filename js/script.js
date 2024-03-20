a0cd7d4dba440417052b775f7b299da09855ca0c
German <98146099+VainGer@users.noreply.github.com>
2024-03-20 18:56:35 +0200

    update 20.3.24

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function openNav() {
+  document.getElementById("mobile-nav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("mobile-nav").style.width = "0%";
+}
+function openMovies() {
+  document.getElementById("movies-menu").style.height = "100%";
+}
+function closeMovies() {
+  document.getElementById("movies-menu").style.height = "0%";
+}
+function openBooks() {
+  document.getElementById("books-menu").style.height = "100%";
+}
+function closeBooks() {
+  document.getElementById("books-menu").style.height = "0%";
+}
+function openHouse() {
+  document.getElementById("house-menu").style.height = "100%";
+}
+function closeHouse() {
+  document.getElementById("house-menu").style.height = "0%";
+}
