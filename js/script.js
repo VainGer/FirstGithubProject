05fd89984f1722a2fdc087e7afd57062a35c1b48
German <98146099+VainGer@users.noreply.github.com>
2024-03-23 22:34:25 +0200

    23.3.24

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 
 function openNav() {
-  document.getElementById("mobile-nav").style.width = "100%";
+  document.getElementById("nav").style.width = "100%";
 }
 function closeNav() {
-  document.getElementById("mobile-nav").style.width = "0%";
+  document.getElementById("nav").style.width = "0%";
 }
 function openMovies() {
-  document.getElementById("movies-menu").style.height = "100%";
+  document.getElementById("movies-menu").style.width = "100%";
 }
 function closeMovies() {
-  document.getElementById("movies-menu").style.height = "0%";
+  document.getElementById("movies-menu").style.width = "0%";
 }
 function openBooks() {
-  document.getElementById("books-menu").style.height = "100%";
+  document.getElementById("books-menu").style.width = "100%";
 }
 function closeBooks() {
-  document.getElementById("books-menu").style.height = "0%";
+  document.getElementById("books-menu").style.width = "0%";
 }
 function openHouse() {
-  document.getElementById("house-menu").style.height = "100%";
+  document.getElementById("house-menu").style.width = "100%";
 }
 function closeHouse() {
-  document.getElementById("house-menu").style.height = "0%";
+  document.getElementById("house-menu").style.width = "0%";
 }
