3b0684cb72e5fb15d6f4200398c1bcab1ffbd531
German <98146099+VainGer@users.noreply.github.com>
2024-03-27 18:16:47 +0200

    desktop

--- js/script.js
+++ js/script.js
@@ -1,70 +1,17 @@
-
-function openNav() {
-  document.getElementById("nav").style.width = "100%";
-}
-function closeNav() {
-  document.getElementById("nav").style.width = "0%";
-}
-function openMovies() {
-  document.getElementById("movies-menu").style.width = "100%";
-}
-function closeMovies() {
-  document.getElementById("movies-menu").style.width = "0%";
-}
-function openBooks() {
-  document.getElementById("books-menu").style.width = "100%";
-}
-function closeBooks() {
-  document.getElementById("books-menu").style.width = "0%";
-}
-function openHouse() {
-  document.getElementById("house-menu").style.width = "100%";
-}
-function closeHouse() {
-  document.getElementById("house-menu").style.width = "0%";
-}
-function openSignup() {
-  document.getElementById("sign-up").style.display="block";
-}
-function closeSignup() {
-  document.getElementById("sign-up").style.display="none";
-}
-function openLogin() {
-  document.getElementById("have-acc").style.display="block";
-}
-function closeLogin() {
-  document.getElementById("have-acc").style.display="none";
+function openAndCloseItemInNav(id){
+  if(document.getElementById(id).style.width === "100%"){
+    document.getElementById(id).style.width = "0%";
+  }
+  else{
+    document.getElementById(id).style.width = "100%";
+  }
 }
 //for home page menu
-function openChar(){
-  if(document.getElementById("characters").style.display==="none"){
-    document.getElementById("characters").style.display="grid";
+function openAccordion(id){
+  if(document.getElementById(id).style.display==="grid"){
+    document.getElementById(id).style.display="none";
   }
   else{
-    document.getElementById("characters").style.display="none";
-  }
-}
-function openMov(){
-  if(document.getElementById("movies").style.display==="none"){
-    document.getElementById("movies").style.display="grid";
-  }
-  else{
-    document.getElementById("movies").style.display="none";
-  }
-}
-function openBook(){
-  if(document.getElementById("books").style.display==="none"){
-    document.getElementById("books").style.display="grid";
-  }
-  else{
-    document.getElementById("books").style.display="none";
-  }
-}
-function openHouses(){
-  if(document.getElementById("houses").style.display==="none"){
-    document.getElementById("houses").style.display="grid";
-  }
-  else{
-    document.getElementById("houses").style.display="none";
+    document.getElementById(id).style.display="grid";
   }
 }
