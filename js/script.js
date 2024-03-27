cfc12c8aadd0ce3e2527d7d03c0267c81d4ac9c1
German <98146099+VainGer@users.noreply.github.com>
2024-03-27 22:07:52 +0200

    update home page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,64 @@
-function openAndCloseItemInNav(id){
-  if(document.getElementById(id).style.width === "100%"){
-    document.getElementById(id).style.width = "0%";
+var desktopWidth = window.matchMedia("(min-width: 999px)");
+var tabletWidth = window.matchMedia("(min-width: 425px)");
+var desktopMaxWidth = window.matchMedia("(max-width: 999px)");
+var tabletMaxWidth = window.matchMedia("(max-width: 425px)");
+function fromMobileToDesktop(mediaWidth) {
+  if (mediaWidth.matches) {
+    location.reload();
   }
-  else{
-    document.getElementById(id).style.width = "100%";
+}
+desktopWidth.addEventListener("change", function () {
+  fromMobileToDesktop(desktopWidth);
+});
+tabletWidth.addEventListener("change", function () {
+  fromMobileToDesktop(tabletWidth);
+});
+desktopMaxWidth.addEventListener("change", function () {
+  fromMobileToDesktop(desktopMaxWidth);
+});
+tabletMaxWidth.addEventListener("change", function () {
+  fromMobileToDesktop(desktopMaxWidth);
+});
+
+// open and close items in nav
+function openAndCloseItemInNav(id) {
+  if (desktopWidth.matches) {
+    if (document.getElementById(id).style.display === "block") {
+      document.getElementById(id).style.display = "none";
+    }
+    else {
+      document.getElementById(id).style.display = "block";
+    }
+  }
+  else {
+    document.getElementById(id).style.display = "block";
+    if (document.getElementById(id).style.width === "100%") {
+      document.getElementById(id).style.width = "0%";
+      document.getElementById(id).style.display = "none";
+    }
+    else {
+      document.getElementById(id).style.width = "100%";
+    }
+  }
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.desktopbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
   }
 }
 //for home page menu
-function openAccordion(id){
-  if(document.getElementById(id).style.display==="grid"){
-    document.getElementById(id).style.display="none";
+function openAccordion(id) {
+  if (document.getElementById(id).style.display === "grid") {
+    document.getElementById(id).style.display = "none";
   }
-  else{
-    document.getElementById(id).style.display="grid";
+  else {
+    document.getElementById(id).style.display = "grid";
   }
 }
