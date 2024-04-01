a3ee076fa7a9d292cff72ab58f25067ae2793c31
German <98146099+VainGer@users.noreply.github.com>
2024-04-01 19:20:44 +0300

    fonts, changes, vars

--- js/script.js
+++ js/script.js
@@ -61,4 +61,34 @@ function openAccordion(id) {
     document.getElementById(id).style.display = "grid";
   }
 }
+//Search
+function search() {
+  document.getElementById("search-list").style.display = "block";
+  if (!desktopWidth.matches) {
+    document.getElementById("closeSearch").style.display = "block";
+  }
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("search-list");
+  li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+  addEventListener("change", function () {
+    closeSearch();
+    document.getElementById("closeSearch").style.display = "none";
+    document.getElementById("search").value = "";
+  });
+}
+function closeSearch() {
+  document.getElementById("search-list").style.display = "none";
+}
+
 
